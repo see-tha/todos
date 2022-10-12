c807980229be62b76f32ea2ea5e3df9c642777e2
see-tha <233757@ust-global.com>
2022-10-12 17:34:46 +0530

    hiiii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todos';
+  title = 'first_project';
+  list:any[]=[];
+   add(item:String)
+    {
+      
+this.list.push({item});
+console.log(this.list)
+
+  }
+  remove(i:number)
+  {
+this.list.splice(i,1);
+  }
 }
